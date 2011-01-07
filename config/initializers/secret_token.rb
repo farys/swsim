63b132f01fe57a76da8343bf650fdb9e053f535b
farys <rafais1@op.pl>
2011-01-07 10:53:12 +0100

    Utworzenie projektu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inz::Application.config.secret_token = '950cf0e9547da63cb751d8e8f7519dfb8517604cc5f69bbe604d68e7e5b230a837fe577b2ed6c5b1c7a43f31715f3bbdb7db4c0eb42e4bacfe2f7f4a4a9e4fc9'
