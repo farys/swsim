22f471e5f046061c4947cfccbad9d469d69e6cad
Maveral <maverral@wp.pl>
2011-03-12 23:24:32 +0100

    - Poprawka

--- app/controllers/project/application_controller.rb
+++ app/controllers/project/application_controller.rb
@@ -3,7 +3,7 @@ class Project::ApplicationController < ApplicationController
   
   def get_project
     @project = Project.find(params[:id])
-    unless @project.member?(@logged_user.id)
+    unless @project.member?(current_user.id)
       flash_t :notice
       redirect_to :controller => '/panel/projects', :action => :index
     end
