1afa03b243eeeaed99c955809eafd114eea32bbc
Maveral <maverral@wp.pl>
2011-03-18 16:07:52 +0100

    - Malutka zmiana lol

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       @country_name = Carmen::country_name(@user.country)
       @country_flag = "flags/#{@user.country.downcase}.gif"
-      #country_flag = "http://www.flagsofallcountries.com/0/#{@user.country.downcase}.gif"
     end
 	
 	def create
