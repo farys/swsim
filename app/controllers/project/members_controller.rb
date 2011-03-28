cbf686e659fbc69b66368d5f70aa97c2dd48ff9d
Yakob <yakob88@o2.pl>
2011-03-28 23:27:22 +0200

    Poprawiono linki ;]

--- app/controllers/project/members_controller.rb
+++ app/controllers/project/members_controller.rb
@@ -1,8 +1,8 @@
 class Project::MembersController < Project::ApplicationController 
-  before_filter :check_privileges, :except => :show
+  before_filter :check_privileges, :except => :index
    
   def index
-    title_t :show
+    title_t :index
   end
   
   def new
@@ -13,15 +13,15 @@ class Project::MembersController < Project::ApplicationController
   	memb = Membership.where(:user_id => params[:membership][:user_id], :project_id => params[:project_id]).first
   	memb.role_id = params[:membership][:role_id].to_i
   	memb.save!
-  	title_t :show
-  	render :index
+  	title_t :index
+  	redirect_to project_members_path
   end
   
   private
   def check_privileges
   	unless can_edit?('user')
-  		title_t :show
-  		render :show
+  		title_t :index
+  		render :index
   	end
   end
 end
