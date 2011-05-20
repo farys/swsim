ef632dc241b8277db46ec6c89c303018eac21a89
Yakob <yakob88@o2.pl>
2011-05-20 16:01:20 +0200

    Kolejne poprawki projektow :P

--- app/controllers/project/application_controller.rb
+++ app/controllers/project/application_controller.rb
@@ -1,24 +1,19 @@
-class Project::ApplicationController < ApplicationController	
+class Project::ApplicationController < ApplicationController
   before_filter :get_project
   before_filter :check_membership, :except =>[:accept, :reject]
+  before_filter :project_active, :except => [:show, :index]
   
-  #sprawdzenie uprawnien do edytowania wybranego widoku  
+  #sprawdzenie uprawnien do edytowania wybranego elementu 
   def can_edit?(page)
-    if @project.active?
-      role = Hash.new(false)
-      role.merge! @project.user_role(current_user.id).attributes
-      role[page]
-	  else
-	  	flash_t_general :notice, 'project.not_active'
-	  	redirect_to project_info_path(@project)
-  	end
+    role = Hash.new(false)
+    role.merge! @project.user_role(current_user.id).attributes
+    role[page]
   end
   
   private
   
-  #autoryzacja uzytkownika jako czlonka projektu
-  def get_project
-       
+  #pobranie projektu
+  def get_project      
     unless Project.exists? params[:project_id]
       flash_t_general :error, 'project.dont_exists'
       redirect_to panel_projects_path
@@ -28,11 +23,19 @@ class Project::ApplicationController < ApplicationController
     @project = Project.find(params[:project_id])
   end
   
+  #autoryzacja uzytkownika jako czlonka projektu
   def check_membership
     unless @project.member?(current_user.id)
       flash_t_general :error, 'project.not_participating'
       redirect_to panel_projects_path
       return
     end    
+  end
+  
+  def project_active
+    unless @project.active?
+      flash_t_general :notice, 'project.not_active'
+	  	redirect_to project_info_path(@project)
+    end
   end
 end
